1537375424f742c2a6fad0b27ce5b7525347906b
morrisaiden <mwaimorris1997@gmail.com>
2023-11-14 09:13:49 -0800

    alias

---
+++ alias_builtin.c
@@ -0,0 +1,31 @@
+#include "alias_builtin.h"
+
+Alias *alias_list = NULL;
+
+/**
+ * add_alias - Adds an alias to the alias_list
+ * @name: The name of the alias
+ * @value: The value associated with the alias
+ */
+void add_alias(const char *name, const char *value)
+{
+
+}
+
+/**
+ * list_aliases - Lists all aliases in the alias_list
+ */
+void list_aliases()
+{
+
+}
+
+/**
+ * execute_alias_builtin - Executes an alias from the alias_list
+ * @argv: The arguments for the execution
+ */
+void execute_alias_builtin(char **argv)
+{
+
+}
+
